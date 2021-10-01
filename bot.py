206ddbe1b7124612b36fdaf5d886eb2cdcc4e68a
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2021-09-30 21:43:49 -0700

    Inserted comments as a guide

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ driver.get("https://www.instagram.com/")
 username = ENTER USER NAME HERE
 password = ENTER PASSWORD HERE
 
-
+# INSTAGRAM LOGIN
 time.sleep(2)
 name_input = driver.find_element_by_name("username")
 name_input.send_keys(username)
@@ -20,6 +20,7 @@ time.sleep(2)
 login = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
 login.click()
 
+# SEARCH QUERY FOR A USER
 time.sleep(2)
 search_query = driver.find_element_by_class_name("XTCLo")
 search_query.send_keys("Naruto")
@@ -27,13 +28,16 @@ time.sleep(2)
 select = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[2]/a')
 select.click()
 
+# GOING THROUGH USER FOLLOWERS
 time.sleep(2)
 check_followers = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
 check_followers.click()
 
+# MAKING A LIST OF FOLLOW BUTTONS
 time.sleep(2)
 follow = driver.find_elements_by_css_selector('button.sqdOP')
 
+# CHECKING BUTTON TEXT
 for i in follow:
     if i.text == "Follow":
         time.sleep(2)
@@ -42,14 +46,19 @@ for i in follow:
     else:
         print("skipped over")
         pass
-
+    
+# EXITING FOLLOWING DIV 
 exit_div = driver.find_element_by_xpath('/html/body/div[6]/div/div/div[1]/div/div[2]/button')
 exit_div.click()
 
+# GRABBING MY PROFILE 
 time.sleep(2)
 go_to_profile = driver.find_element_by_css_selector('span._2dbep.qNELH')
 go_to_profile.click()
 
+# RENDERING MY PAGE
 time.sleep(2)
 render_profile = driver.find_element_by_css_selector('div._01UL2>a')
 render_profile.click()
+
+driver.close()
